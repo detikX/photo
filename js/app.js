c39d29e968882798cd9626741e6392f1e58aa348
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-08-05 13:00:16 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,27 @@
-$(window)
-  .scroll(function () {
-    let o = $(window),
-      i = $("body"),
-      t = $(".section");
-    s = o.scrollTop() + o.height() / 3;
-    t.each(function () {
-      console.log("top", $(this).position().top);
-      console.log("s", s);
-      $(this).position().top <= s &&
-        $(this).position().top + $(this).height() > s &&
-        i.removeClass(
-          function (o, i) {
-            return i.match(/(^|\s)color-\s+/g) || [].join(" ");
-          },
-          $(".section").removeClass("active"),
-          $(this).addClass("active")
-        );
-    });
-  })
-  .scroll();
+$(window).on("load", function () {
+  $(".preloader").fadeOut(1e4), $(".preloader").remove();
+}),
+  $(window)
+    .scroll(function () {
+      let o = $(window),
+        i = $("body"),
+        t = $(".section");
+      s = o.scrollTop() + o.height() / 3;
+      t.each(function () {
+        console.log("top", $(this).position().top);
+        console.log("s", s);
+        $(this).position().top <= s &&
+          $(this).position().top + $(this).height() > s &&
+          i.removeClass(
+            function (o, i) {
+              return i.match(/(^|\s)color-\s+/g) || [].join(" ");
+            },
+            $(".section").removeClass("active"),
+            $(this).addClass("active")
+          );
+      });
+    })
+    .scroll();
 
 const root = document.querySelector("html");
 
