d6a185076143d6713724bae38eddfc7df00afdfd
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-09-07 16:41:19 +0700

    update

--- js/app.js
+++ js/app.js
@@ -23,12 +23,33 @@ $(window).on("load", function () {
     })
     .scroll();
 
+    var $slickElement = $('.slider-pict');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        // $status.text(i + '/' + slick.slideCount);
+        $(".now").text(i);
+        $(".totalx").text(slick.slideCount)
+    });
+
     $('.slider-pict').slick({
       dots: true,
       infinite: true,
       speed: 500,
       fade: true,
-      cssEase: 'linear'
+      cssEase: 'linear',
+      // customPaging: function (slider, i) {
+      //     //FYI just have a look at the object to find available information
+      //     //press f12 to access the console in most browsers
+      //     //you could also debug or look in the source
+      //     // console.log(slider);
+      //     // return  (i + 1) + '/' + slider.slideCount;
+      //     var i = (slider.currentSlide ? slider.currentSlide : 0) + 1;
+      //     console.log(i);
+      //     $(".now").text(i);
+      //     $(".totalx").html(slider.slideCount)
+      // }
     });
 
     $(".close-info").click(()=>{
