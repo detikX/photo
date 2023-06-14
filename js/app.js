712ab1fea73c2c3889b41186c7dba5fc5a3b30a7
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-06-14 16:20:36 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -142,19 +142,3 @@ btns.forEach((btn) => {
     
   }
 })*/
-
-gsap.registerPlugin(ScrollTrigger);
-
-let sections = gsap.utils.toArray(".panel");
-
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".containerx",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => "+=" + document.querySelector(".containerx").offsetWidth
-  }
-});
