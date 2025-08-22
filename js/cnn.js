6db5e26aceba3bb73cf44c7ee5ff6b604a82a853
Dedi Arief Wibisono <dedi.wibisono@Dedis-MacBook-Pro.local>
2025-08-22 16:53:32 +0700

    update

--- js/cnn.js
+++ js/cnn.js
@@ -35,6 +35,16 @@ function onChange(event) {
 
 
 
+var $slickElement = $('.sliderx');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  // $status.text(i + '/' + slick.slideCount);
+  $(".now").text(i);
+  $(".totalx").text(slick.slideCount)
+});
+
 
 // query selector all returns a list of nodes, therefore we need to iterate over it and attach an event listener to each button seperatly
 btns.forEach((btn) => {
